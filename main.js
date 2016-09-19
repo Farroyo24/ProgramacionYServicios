f864f6b3b43c87f95c6c3b1d74c4da290ac9679f
matinal <matinal@matinal>
2016-09-19 14:13:04 +0200

    Primer día Programación y Servicios

---
+++ main.js
@@ -0,0 +1,9 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+console.log("Hola mundo");
+var Hola = "Hola";
+console.log(Hola);
+
